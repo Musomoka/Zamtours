3dd4b708933e83a43aa595eaabbd5c0a70dbac76
Musomoka <musosum@gmail.com>
2017-11-20 20:35:01 +0200

    pushing to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171120181501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tour_items", force: :cascade do |t|
     t.string "title"
     t.string "category"
